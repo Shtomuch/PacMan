568856f611dcc8c02bf6a2e0a6944fd26c1c668f
IrusHunter <korinevskiy.roman@gmail.com>
2025-02-16 16:29:24 +0200

    adding 4 Ghost classes: Blinky, Pinky, Inky, Clyde

--- classes/ghost.py
+++ classes/ghost.py
@@ -193,3 +193,120 @@ class Ghost:
 
         self.move_unit.move(delta, self.direction, is_ghost=ghost)
         self.time_to_drop = False
+
+
+class Blinky(Ghost):
+    """
+    Червоний привид, який агресивно переслідує Пакмена
+    """
+
+    def __init__(self, coordinates) -> None:
+        super().__init__(coordinates, ghost_type=0)
+        self.turn_alive()
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, value) -> None:
+        self._target = copy.deepcopy(value)
+
+
+class Pinky(Ghost):
+    """
+    Розовий привид, який переслідує Пакмена, націлившись на 4 клітинку перед ним
+    """
+
+    def __init__(self, coordinates) -> None:
+        super().__init__(coordinates, ghost_type=1)
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, value) -> None:
+        self._target = copy.deepcopy(value)
+        if self.state in ("in_house", "dead"):
+            return
+
+        offset = 4 * GlobalVars.tile_size
+        direction = GlobalVars.pacman.move_unit.direction
+
+        if direction == 0:
+            self._target.x_global += offset
+        elif direction == 1:
+            self._target.y_global += offset
+        elif direction == 2:
+            self._target.x_global -= offset
+        elif direction == 3:
+            self._target.y_global -= offset
+
+
+class Inky(Ghost):
+    """
+    Бірюзовий привид, який вибирає координати цілі посередині між Блінкі та Пакменом,
+    віддзеркаливши їх відносно координат Блінкі
+    """
+
+    def __init__(self, coordinates) -> None:
+        super().__init__(coordinates, ghost_type=2)
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, value) -> None:
+        self._target = copy.deepcopy(value)
+        if self.state in ("in_house", "dead"):
+            self._target.x_global -= GlobalVars.tile_size * 2
+            return
+
+        blinky_pos = GlobalVars.ghosts[0].move_unit.coordinates
+        mid_x = (self._target.x_global + blinky_pos.x_global) / 2
+        mid_y = (self._target.y_global + blinky_pos.y_global) / 2
+
+        self._target.x_global = 2 * blinky_pos.x_global - mid_x
+        self._target.y_global = 2 * blinky_pos.y_global - mid_y
+
+
+class Clyde(Ghost):
+    """
+    Оранжевий привид, який тікає від Пакмена, але наближується до нього, коли занадто далеко втік
+    """
+
+    def __init__(self, coordinates) -> None:
+        self.coward: bool = False
+        self.coward_distance: int = 8
+        self.angry_distance: int = 16
+        super().__init__(coordinates, ghost_type=3)
+
+    @property
+    def target(self):
+        return self._target
+
+    @target.setter
+    def target(self, value) -> None:
+        self._target = copy.deepcopy(value)
+        if self.state in ("in_house", "dead"):
+            self._target.x_global += GlobalVars.tile_size * 2
+            return
+
+        dist = abs(self._target.x_global - self.move_unit.coordinates.x_global) + \
+               abs(self._target.y_global - self.move_unit.coordinates.y_global)
+
+        if self.coward:
+            if dist >= GlobalVars.tile_size * self.angry_distance:
+                self.coward = False
+
+            delta = GlobalVars.tilemap.width * GlobalVars.tilemap.height
+            self._target.x_global += (
+                                         -1 if self._target.x_global > self.move_unit.coordinates.x_global else 1) * GlobalVars.tile_size * delta
+            self._target.y_global += (
+                                         -1 if self._target.y_global > self.move_unit.coordinates.y_global else 1) * GlobalVars.tile_size * delta
+        else:
+            if dist <= GlobalVars.tile_size * self.coward_distance:
+                self.coward = True
+
